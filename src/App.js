18910d6fee0b0a4e7ed869c9459669b653711c99
williamyap90 <william.yap90@gmail.com>
2021-11-16 14:39:40 +0000

    refactor to class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>hello</h1>
-        </div>
-    );
+class App extends React.Component {
+    render() {
+        return <h1>To Do</h1>;
+    }
 }
 
 export default App;
