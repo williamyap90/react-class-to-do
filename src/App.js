e0da83a93661f4171ee4de7c8ccc0f108a15193b
williamyap90 <william.yap90@gmail.com>
2021-11-16 16:02:54 +0000

    add props and pass to ToDoList

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import ManageTasks from './components/ManageTasks';
 import ToDoList from './components/ToDoList';
+import data from './data.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDo: data,
+    };
+  }
+
   render() {
     return (
       <div>
         <Header />
         <ManageTasks />
-        <ToDoList />
+        <ToDoList toDo={this.state.toDo} />
       </div>
     );
   }
