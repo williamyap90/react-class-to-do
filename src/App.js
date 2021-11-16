7695b0ff416604b7c0e27e233be47ddab35af870
williamyap90 <william.yap90@gmail.com>
2021-11-16 15:28:13 +0000

    rename component for clarity

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import React, { Component } from 'react';
 import Header from './components/Header';
-import CreateTask from './components/CreateTask';
+import ManageTasks from './components/ManageTasks';
 import List from './components/List';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <CreateTask />
-                <List />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <ManageTasks />
+        <List />
+      </div>
+    );
+  }
 }
 
 export default App;
