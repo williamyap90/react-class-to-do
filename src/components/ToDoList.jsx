279ab0e74d12d5b4fe53ca2fbddc69a12d798779
williamyap90 <william.yap90@gmail.com>
2021-11-17 10:36:45 +0000

    add display for number of tasks remaining

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,9 +4,10 @@ import ToDo from './ToDo';
 class ToDoList extends Component {
   render() {
     const { toDo } = this.props;
+
     return (
       <div>
-        <h2>Outstanding</h2>
+        <h2>{toDo.length} tasks remaining</h2>
         {toDo.map((item) => {
           return <ToDo key={item.id} toDo={item} />;
         })}
