90063539ba3e3a9d10fc3ef990f41f539655a393
williamyap90 <william.yap90@gmail.com>
2021-11-17 14:39:41 +0000

    change to display unchecked tasks only

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -7,7 +7,7 @@ class ToDoList extends Component {
 
     return (
       <div>
-        <h2>{toDo.length} tasks remaining</h2>
+        <h2>{toDo.filter((item) => item.complete !== true).length} tasks remaining</h2>
         {toDo.map((item) => {
           return <ToDo key={item.id} toDo={item} removeTask={removeTask} handleChecked={handleChecked} />;
         })}
