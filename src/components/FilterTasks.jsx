4015afdc8e2a23517e72fd2a7ade4ba167c98e44
williamyap90 <william.yap90@gmail.com>
2021-11-17 09:01:07 +0000

    shorten button labels

--- src/components/FilterTasks.jsx
+++ src/components/FilterTasks.jsx
@@ -4,9 +4,9 @@ class FilterTasks extends Component {
   render() {
     return (
       <div>
-        <button>Show all tasks</button>
-        <button>Show active tasks</button>
-        <button>Show completed tasks</button>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
       </div>
     );
   }
