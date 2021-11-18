bacfc21da4a6d0f13b338245808d55e94cde5c23
williamyap90 <william.yap90@gmail.com>
2021-11-18 16:01:21 +0000

    add conditional render for outstanding tasks

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -5,9 +5,16 @@ class ToDoList extends Component {
   render() {
     const { toDo, removeTask, handleChecked } = this.props;
 
+    const outstandingTasks = toDo.filter((item) => item.complete !== true);
+
     return (
       <div>
-        <h2>{toDo.filter((item) => item.complete !== true).length} tasks remaining</h2>
+        {outstandingTasks.length ? (
+          <h2>{outstandingTasks.length} tasks remaining</h2>
+        ) : (
+          <h2>You have no tasks outstanding</h2>
+        )}
+
         {toDo.map((item) => {
           return <ToDo key={item.id} toDo={item} removeTask={removeTask} handleChecked={handleChecked} />;
         })}
