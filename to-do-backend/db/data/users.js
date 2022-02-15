55ed15f3e3b30405e4c76cf439eb6db11b83b53f
williamyap90 <william.yap90@gmail.com>
2022-02-15 09:27:48 +0000

    add sample data

---
+++ to-do-backend/db/data/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    username: 'mallionaire',
+    first_name: 'haz',
+    last_name: 'harrison',
+    avatar_url: 'https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/avatar-default-icon.png',
+  },
+  {
+    username: 'philippaclaire9',
+    first_name: 'philipa',
+    last_name: 'claire',
+    avatar_url: 'https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/avatar-default-icon.png',
+  },
+  {
+    username: 'bainesface',
+    first_name: 'sarah',
+    last_name: 'bains',
+    avatar_url: 'https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/avatar-default-icon.png',
+  },
+  {
+    username: 'dav3rid',
+    first_name: 'dave',
+    last_name: 'rid',
+    avatar_url: 'https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/avatar-default-icon.png',
+  },
+];
